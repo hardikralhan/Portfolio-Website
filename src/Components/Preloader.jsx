510c7d69d94f929a5706d62150cded876f06098e
Hardik <hardikralhan66@gmail.com>
2022-11-14 16:57:17 +0530

    resume changed

--- src/Components/Preloader.jsx
+++ src/Components/Preloader.jsx
@@ -1,4 +1,4 @@
-import pic from "../HR-logo.jpg";
+import icon from "../HR-logo.jpg";
 
 
 function Preloader(){
@@ -7,7 +7,7 @@ function Preloader(){
     <section data-scroll-section>
         <div className="h-screen flex flex-col items-center justify-center bg-black text-zinc-200 absolute top-0 bottom-0 left-0 right-0">
         <div className="align-middle">
-        <img src={pic} alt="HR im"></img>
+        <img src={icon} alt="HR im"></img>
         </div>
         <div className="align-middle">
             <h1 className="-mt-11 font-Inknut text-4xl">HARDIK RALHAN</h1>
